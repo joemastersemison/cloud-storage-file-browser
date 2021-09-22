288d92358b63d322bf222e49a4e324cd0441af4d
joe <joe@ourbranch.com>
2021-09-22 03:31:11 +0000

    ignoring warnings

--- app/src/components/FileCard/FileCard.js
+++ app/src/components/FileCard/FileCard.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import "./FileCard.css";
 import { Card, List, Button, Icon, Dropdown, Dimmer } from "semantic-ui-react";
--- app/src/components/FileExplorer/FileExplorer.js
+++ app/src/components/FileExplorer/FileExplorer.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import "./FileExplorer.css";
 import {
